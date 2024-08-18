d0091db2dcf49d23bc50a10c5dfcc46a61e7ebc5
Garvit <heisGarvit@gmail.com>
2024-08-18 18:43:32 +0530

    Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,49 +1,39 @@
 // src/components/Notes.jsx
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Notes from "./components/Notes/Notes";
 import Code from "./components/Code/Code";
 import Assignment from "./components/Assignment/Assignment";
 import CourseInstructor from "./components/instructor_coding_assignment/Code_instructor";
 import Instructor from "./components/instructor_GA/GA_instructor";
 import FlashcardList from "./components/Flashcard/FlashcardList";
-import Biology from "./components/Notes/Biology";
-import Math from "./components/Notes/Math";
-import English from "./components/Notes/English";
-import ComputerScience from "./components/Notes/ComputerScience.jsx";
 import InstructorNotes from "./components/instructor_Notes/InstructorNotes.jsx";
 import Login from "./components/Login/Login";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Notes />} />
-          <Route path="/notes/:courseId/:courseName" element={<Notes />} />
-          <Route path="/assignment" exact element={<Assignment />}></Route>
-          <Route path="/code" exact element={<Code />}></Route>
-          <Route path="/flashcard" exact element={<FlashcardList />} />
-          {/* <Route path="/notes/biology" exact element={<Biology />} />
-          <Route path="/notes/math" exact element={<Math />} />
-          <Route path="/notes/english" exact element={<English />} />
-          <Route
-            path="/notes/computer-science"
-            exact
-            element={<ComputerScience />}
-          /> */}
-          <Route
-            path="/instructor/code"
-            exact
-            element={<CourseInstructor />}
-          ></Route>
-          <Route path="/login" exact element={<Login />}></Route>
-          <Route path="/instructor/GA" exact element={<Instructor />}></Route>
-          <Route path="/instructor/notes" exact element={<InstructorNotes />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <PrimeReactProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact element={<Notes/>}/>
+                    <Route path="/notes/:courseId/:courseName" element={<Notes/>}/>
+                    <Route path="/assignment" exact element={<Assignment/>}></Route>
+                    <Route path="/code" exact element={<Code/>}></Route>
+                    <Route path="/flashcard" exact element={<FlashcardList/>}/>
+                    <Route
+                        path="/instructor/code"
+                        exact
+                        element={<CourseInstructor/>}
+                    ></Route>
+                    <Route path="/login/:token/type/:user_type" exact element={<Login/>}></Route>
+                    <Route path="/login" exact element={<Login/>}></Route>
+                    <Route path="/instructor/GA" exact element={<Instructor/>}></Route>
+                    <Route path="/instructor/notes" exact element={<InstructorNotes/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </PrimeReactProvider>
+    );
 };
 
 export default App;
--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,127 +1,60 @@
-import React, { useState, useEffect, useRef } from "react";
-import Sidenav from "../Sidenav";
-import { Button, FormControl, Select, MenuItem } from "@mui/material";
-// import FlashcardGenerateButton from '../FlashcardGenerateButton'
+import React from "react";
 import "../../App.css";
-import axios from "axios";
-import { Box } from "@mui/material";
+import {Navigate, useParams} from "react-router-dom";
+import { Button } from 'primereact/button';
 
-const FlashcardGenerateButton = () => {
-    const handleGenerate = () => {
-        // Logic for downloading PDF
-        alert("Generating Flashcards...");
-    };
-    return (
-        <Button variant="contained" color="primary" onClick={handleGenerate}>
-            Generate
-        </Button>
-    );
-};
+const LoginPage = (props) => {
+    const params = useParams()
+    const token = params.token
+    const user_type = params.user_type
 
-// function handleChange(e){
-//     e.preventDefault()
 
-const SAMPLE_FLASHCARDS = [
-    {
-        id: 1,
-        subject: "Biology",
-        question: "Mitochondria",
-        answer:
-            'Mitochondria are membrane-bound organelles present in the cytoplasm of all eukaryotic cells that produce adenosine triphosphate (ATP), the main energy molecule used by the cell. They are known as the "Powerhouse of the cell.',
-    },
-    {
-        id: 2,
-        question: "Mitochondrial Disorders",
-        subject: "Biology",
-        answer: "Alpers Disease, Barth Syndrome, and Kearns-Sayre syndrome (KSS)",
-    },
-    {
-        id: 2,
-        question: "Functions of Mitochondria",
-        subject: "Biology",
-        answer:
-            "Beyond energy production, mitochondria regulate metabolism, promote cell growth and multiplication, detoxify ammonia in liver cells, facilitate apoptosis, build blood and hormones, maintain calcium levels, and are involved in cellular differentiation, signaling, senescence, and cell cycle control.",
-    },
-    {
-        id: 2,
-        question: "Structure of Mitochondria",
-        subject: "Biology",
-        answer:
-            "he mitochondrion is a double-membraned, rod-shaped structure found in both plant and animal cells, ranging from 0.5 to 1.0 micrometre in diameter. ",
-    },
-    {
-        id: 2,
-        question: "What is 2+2?",
-        subject: "Math",
-        answer: "4",
-    },
-    {
-        id: 3,
-        question: "Colors?",
-        answer: "VIBGYOR",
-        subject: "Computer Science",
-    },
-    {
-        id: 4,
-        question: "What is a Membrane?",
-        subject: "English",
-        answer:
-            "A membrane is a selective barrier; it allows some things to pass through but stops others. Such things may be molecules, ions, or other small particles. Membranes can be generally classified into synthetic membranes and biological membranes.",
-    },
-];
+    if (token && token.length > 20) {
+        localStorage.setItem("token", params.token)
+        localStorage.setItem("user_type", user_type)
+        if (user_type === "student") {
+            Navigate({to: '/'})
 
-// }
+        }
+        else if (user_type === "instructor") {
+            Navigate({to: '/instructor/GA'})
+        }
+    }
 
-const FlashcardList = () => {
-    const [flashcards, setFlashcards] = useState([]);
-    const [allCourses, setAllCourses] = useState({});
-    const [subject, setSubject] = useState("Select a Course");
-
-    useEffect(() => {
-        axios.get('http://localhost:8000/flash_card/getall')
-            .then(res => {
-                setFlashcards(res.data);
-            })
-            .catch(err => {
-                console.error("Error fetching flashcards:", err);
-            });
-    }, []);
-
-    useEffect(() => {
-        axios.get("http://localhost:8000/course/getall")
-            .then((res) => {
-                const courses = {};
-                res.data.forEach(course => {
-                    courses[course.course_id] = course.course_name;
-                });
-                setAllCourses(courses);
-            })
-            .catch(err => {
-                console.error("Error fetching courses:", err);
-            });
-    }, []);
-
-    const handleChange = (event) => {
-        setSubject(event.target.value);
-    };
-
-    const categoryEl = useRef();
-
-    // const [curr_category, setCategory] = useState("Select a Course");
-
-    // useEffect(() => {
-    //   axios
-    //    .get('https://opentdb.com/api_category.php')
-    //    .then(res => {
-    //     setCategories(res.data.trivia_categories)
-    //    })
-    // })
+    const login = () => {
+        console.log("login")
+        window.open(process.env.REACT_APP_BACKEND_URL + 'login/google', '_self')
+    }
 
     return (
         <>
-            <div>Hello Welcome to study </div>
+            <div className="md:h-36"></div>
+            <div className="max-w-6xl bg-lime-50 m-auto rounded-lg pt-2 md:pb-2 pb-10 px-2">
+                <div className="divide-lime-400 lg:grid lg:grid-cols-2 lg:divide-x">
+                    <div className="h-96 m-auto flex flex-col align-middle justify-center">
+                        <img alt="logo" className="h-48 object-contain" src={require('../../assets/logo.png')}/>
+                        <div className="text-xl pt-5 text-center">
+                            Welcome To Study Buddy
+                        </div>
+                    </div>
+                    <div className="h-96 ">
+                        <div className="flex flex-col align-middle justify-center h-80">
+                            <div className="mx-auto text-2xl">
+                                Hi
+                            </div>
+                            <div className="mx-auto mb-3 mt-10 text-xl">
+                                <div className="text-center mb-5">Continue to login</div>
+                                <Button label="Login With Google" icon="pi pi-check" className="mt-5 w-full" onClick={login}/>
+                            </div>
+                            <div className="mx-auto w-80">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
 
-export default FlashcardList;
+export default LoginPage;
